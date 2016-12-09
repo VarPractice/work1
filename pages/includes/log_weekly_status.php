53dbd0c72e68206c6cdbf328a1e3e106ab7d770d
VattikutiCognizant <499787@PC305831.cts.com>
2016-12-09 18:15:58 +0530

    Add code to replace status report if it is duplicate report logged

--- pages/includes/log_weekly_status.php
+++ pages/includes/log_weekly_status.php
@@ -43,9 +43,21 @@
 	// 	Connecting To db
 	$con=create_sqlConnection();
 	// Setting the query string
-	$sts_log_query="insert into project_maritz(`for week`,`project id`,`point of contact`,`team_size`,`sprint_end_date`,`current work`, `future work`, `challenges`, `quality`, `impact`, `execution`, `design`, `ramupdown`, `appreciation`, `last updated by`)
+	// Checking wehther status is already logged for current project
+	$res=exeQuery("select `report id` from `project_maritz` where `for week`='$cur_week_number' and `project id`='$project_id'",$con);
+	if($res->num_rows > 0)
+	{
+		$row = $res->fetch_assoc(); // As we alaways get only one row result set, no need of looping statements.
+		$report_id=$row['report id'];
+		// This will replace the existing status & insert current status
+		$sts_log_query="update `project_maritz` set `point of contact`='$prj_poc',`team_size`='$team_size',`sprint_end_date`='$sprnt_date',`current work`='$cur_wrk', `future work`='$futr_wrk', `challenges`='$prj_chlng_desc', `quality`='$prj_qlty', `impact`='$prj_impact', `execution`='$tst_exe', `design`='$tst_dsgn', `ramupdown`='$ram_up_dwn', `appreciation`='$appr_fl_pth', `last updated by`='$emp_id' where `report id`='$report_id'";
+	}
+	else
+	{
+		// This will log new status
+		$sts_log_query="insert into project_maritz(`for week`,`project id`,`point of contact`,`team_size`,`sprint_end_date`,`current work`, `future work`, `challenges`, `quality`, `impact`, `execution`, `design`, `ramupdown`, `appreciation`, `last updated by`)
 	values('$cur_week_number','$project_id', '$prj_poc','$team_size','$sprnt_date', '$cur_wrk', '$futr_wrk', '$prj_chlng_desc', '$prj_qlty', '$prj_impact', '$tst_exe', '$tst_dsgn', '$ram_up_dwn', '$appr_fl_pth', '$emp_id')";
-
+	}
 	// Executing Query to get login details
 	$res=exeQuery($sts_log_query,$con);
 	// 	Varifying wether data is saved or not *** $con->insert_id returns the Id of last inserted record only if the data is successfully stored to DB
